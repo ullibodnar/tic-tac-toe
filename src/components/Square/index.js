f05acd3e81b0b94ca6d2818fa301165661513a5e
Ulli Bodnar <ullibodnar@gmail.com>
2018-06-06 21:25:08 +1200

    Improve the board squares

---
+++ src/components/Square/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledSquare = styled.div`
+  border-color: hsla(0, 0%, 0%, 0.2);
+  border-style: solid;
+  border-width: 0 ${props => (props.index % 3 === 2 ? 0 : '2px')}
+    ${props => (props.index < 6 ? '2px' : 0)} 0;
+  color: ${props =>
+    props.player === 'x' ? 'hsla(6, 59%, 50%, 1)' : 'hsla(145, 63%, 32%, 1)'};
+  font-size: 16vh;
+  font-weight: bold;
+  line-height: 20vh;
+  text-align: center;
+  text-transform: uppercase;
+`
+StyledSquare.displayName = 'StyledSquare'
+
+export default function Square (props) {
+  return (
+    <StyledSquare index={props.index} player={props.player}>
+      {props.player}
+    </StyledSquare>
+  )
+}
