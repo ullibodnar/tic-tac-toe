28762ff1aa8f7c3ccca46a7d323c36483294adfa
Ulli Bodnar <ullibodnar@gmail.com>
2018-06-10 13:25:08 +1200

    Mark the winning squares

--- src/components/Square/index.js
+++ src/components/Square/index.js
@@ -1,3 +1,4 @@
+// src/components/Square/index.js
 import React from 'react'
 import styled from 'styled-components'
 import { isUndefined } from 'ramda-adjunct'
@@ -7,9 +8,7 @@ const StyledSquare = styled.div`
   border-style: solid;
   border-width: 0 ${({ index }) => (index % 3 === 2 ? 0 : '2px')}
     ${({ index }) => (index < 6 ? '2px' : 0)} 0;
-  color: ${({ player }) =>
-    player === 'x' ? 'hsla(6, 59%, 50%, 1)' : 'hsla(145, 63%, 32%, 1)'};
-  cursor: ${({ onClick }) => (isUndefined(onClick) ? 'default' : 'pointer')}
+  cursor: default;
   font-size: 16vh;
   font-weight: bold;
   line-height: 20vh;
@@ -18,12 +17,49 @@ const StyledSquare = styled.div`
 `
 StyledSquare.defaultName = 'StyledSquare'
 
-export default function Square ({ handleClick, index, player }) {
-  return isUndefined(player) ? (
-    <StyledSquare index={index} onClick={handleClick} />
-  ) : (
-    <StyledSquare index={index} player={player}>
+const SquarePlayed = StyledSquare.extend`
+  color: ${({ player }) =>
+    player === 'x' ? 'hsla(6, 59%, 50%, 1)' : 'hsla(145, 63%, 32%, 1)'};
+`
+SquarePlayed.defaultName = 'SquarePlayed'
+
+const SquareLost = StyledSquare.extend`
+  color: hsla(0, 0%, 90%, 1);
+`
+SquareLost.defaultName = 'SquareLost'
+
+const SquarePlayable = StyledSquare.extend`
+  cursor: pointer;
+`
+SquarePlayable.defaultName = 'SquarePlayable'
+
+export default function Square ({
+  handleClick,
+  index,
+  isWinningSquare,
+  player
+}) {
+  if (isUndefined(isWinningSquare)) {
+    return isUndefined(player) ? (
+      <SquarePlayable index={index} onClick={handleClick} />
+    ) : (
+      <SquarePlayed index={index} player={player}>
+        {player}
+      </SquarePlayed>
+    )
+  }
+
+  if (isUndefined(player)) {
+    return <StyledSquare index={index} />
+  }
+
+  return isWinningSquare ? (
+    <SquarePlayed index={index} player={player}>
       {player}
-    </StyledSquare>
+    </SquarePlayed>
+  ) : (
+    <SquareLost index={index} player={player}>
+      {player}
+    </SquareLost>
   )
 }
--- src/components/Square/index.spec.js
+++ src/components/Square/index.spec.js
@@ -1,42 +1,60 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import configureStore from 'redux-mock-store'
 
 import Square from '.'
+import { initialState, SQUARE_CLICKED } from '../../state'
 
-describe('components:Square', () => {
-  it('renders the Square with the proper cursor if a click handler is provided', () => {
-    expect(
-      toJson(shallow(<Square handleClick={() => null} index={0} />).dive())
-    ).toMatchSnapshot()
+const mockStore = configureStore()
+
+describe('containers:Square', () => {
+  it(`maps state and dispatch to props`, () => {
+    const square = 4
+    const store = mockStore({ moves: [0, 3, square] })
+    const wrapper = shallow(<Square index={square} store={store} />)
+
+    expect(wrapper.props()).toEqual(
+      expect.objectContaining({
+        player: 'x',
+        handleClick: expect.any(Function)
+      })
+    )
   })
 
-  it('renders the Square with the proper styles for player O in the top left square', () => {
-    expect(
-      toJson(shallow(<Square player='o' index={0} />).dive())
-    ).toMatchSnapshot()
+  it(`maps state properly to props when the game is over`, () => {
+    const square = 4
+    const store = mockStore({
+      moves: [0, 1, 4, 5, 8],
+      winningSquares: [0, 4, 8],
+      winningPlayer: 'x'
+    })
+    const wrapper = shallow(<Square index={square} store={store} />)
+
+    expect(wrapper.props()).toEqual(
+      expect.objectContaining({
+        isWinningSquare: true
+      })
+    )
   })
 
-  it('renders the Square with the proper styles for player X in the top left square', () => {
-    expect(
-      toJson(shallow(<Square player='x' index={0} />).dive())
-    ).toMatchSnapshot()
-  })
+  it(`maps handleClick to dispatch ${SQUARE_CLICKED} action`, () => {
+    const square = 4
+    const store = mockStore(initialState)
 
-  it('renders the Square with the proper styles for player X in the top right square', () => {
-    expect(
-      toJson(shallow(<Square player='x' index={2} />).dive())
-    ).toMatchSnapshot()
-  })
+    store.dispatch = jest.fn()
 
-  it('renders the Square with the proper styles for player X in the bottom left square', () => {
-    expect(
-      toJson(shallow(<Square player='x' index={6} />).dive())
-    ).toMatchSnapshot()
-  })
+    const wrapper = shallow(<Square index={square} store={store} />)
 
-  it('renders the Square with the proper styles for player X in the bottom right square', () => {
-    expect(
-      toJson(shallow(<Square player='x' index={8} />).dive())
-    ).toMatchSnapshot()
+    wrapper
+      .dive()
+      .props()
+      .onClick()
+
+    expect(store.dispatch).toHaveBeenCalledWith({
+      type: SQUARE_CLICKED,
+      payload: {
+        square
+      }
+    })
   })
 })
